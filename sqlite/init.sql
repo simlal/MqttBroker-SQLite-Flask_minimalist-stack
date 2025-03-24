4f3a23ab77a1d4f96a1b72acdae04a111b49ff1e
simdeck <simlalonde@hotmail.com>
2025-03-24 07:33:45 -0400

    Update sqlite schema with default devices insert

--- sqlite/init.sql
+++ sqlite/init.sql
@@ -1,8 +1,23 @@
 CREATE TABLE esp32_temperatures (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    device_id TEXT NOT NULL,
+    mesh_id INTEGER REFERENCES devices_info(id),
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
     temperature REAL NOT NULL,
     received_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    gateway_id INTEGER REFERENCES devices_info(id),
     gateway_rssi INTEGER
 );
+
+CREATE TABLE devices_info (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    internal_id INTEGER NOT NULL,
+    mac_address TEXT NOT NULL UNIQUE,
+    chip TEXT CHECK(chip IN ('ESP32-WROOM', 'ESP32-WROOM-32D')) NOT NULL,
+    info TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- insert devices used for project
+INSERT INTO devices_info (internal_id, mac_address, chip, info) VALUES (0, "3C:E9:0E:72:12:4C", "ESP32-WROOM", "NORVI_IIOT_GATEWAY");
+INSERT INTO devices_info (internal_id, mac_address, chip, info) VALUES (4, "40:91:51:CB:A4:64", "ESP32-WROOM-32D", "MESH_TEMPERATURE_SENSOR");
+INSERT INTO devices_info (internal_id, mac_address, chip, info) VALUES (6, "E0:5A:1B:30:B3:38", "ESP32-WROOM-32D", "MESH_TEMPERATURE_SENSOR");
